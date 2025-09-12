ae431d4ff2b5dc4a4a140a3243a3abfd1807498c
Amirmahdi Nourkazemi <nourkazemi80@gmail.com>
2025-09-12 19:46:00 +0330

    feat: add ai list items

--- lib/feature/home/presentation/pages/home_screen.dart
+++ lib/feature/home/presentation/pages/home_screen.dart
@@ -1,6 +1,10 @@
+import 'package:appro_chat/core/theme/svg_icon.dart';
+import 'package:appro_chat/core/theme/theme.dart';
 import 'package:appro_chat/core/widgets/app_bar.dart';
 import 'package:appro_chat/core/widgets/drawer_widget.dart';
+import 'package:appro_chat/feature/home/presentation/widgets/ai_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -19,10 +23,84 @@ class _HomeScreenState extends State<HomeScreen> {
       key: scaffoldKey,
       endDrawer: const DrawerWidget(),
       appBar: appBar(key: scaffoldKey),
-      body: ListView(
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Lottie.asset('assets/json/ai.json',
+                  height: MediaQuery.of(context).size.height * 0.3),
+              GridView.count(
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                physics:
+                    const NeverScrollableScrollPhysics(), // prevent scroll conflict
+                mainAxisSpacing: Dimensions.marginSmall,
+                crossAxisSpacing: Dimensions.marginSmall,
+                children: [
+                  for (var item in aiList.values)
+                    AiContainer(
+                      title: item['name'],
+                      svgIcon: item['image'],
+                    ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AiContainer extends StatelessWidget {
+  final String title;
+  final String svgIcon;
+  const AiContainer({
+    super.key,
+    required this.title,
+    required this.svgIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      width: 75,
+      child: Column(
         children: [
-          Lottie.asset('assets/json/ai.json',
-              height: MediaQuery.of(context).size.height * 0.3)
+          Container(
+            height: 75,
+            width: 75,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+                borderRadius:
+                    BorderRadius.circular(Dimensions.cornerRadiusMedium),
+                shape: BoxShape.rectangle,
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Theme.of(context).colorScheme.primary,
+                      Theme.of(context).colorScheme.primary.withOpacity(0.3)
+                    ])),
+            child: SvgPicture.string(
+              svgIcon,
+              height: 20,
+              width: 20,
+            ),
+          ),
+          const SizedBox(
+            height: Dimensions.marginSmall,
+          ),
+          Text(
+            title,
+            style: Theme.of(context)
+                .textTheme
+                .titleSmall!
+                .copyWith(fontWeight: FontWeight.bold, fontSize: 11),
+            textDirection: TextDirection.rtl,
+          )
         ],
       ),
     );
---
+++ lib/feature/home/presentation/widgets/ai_list.dart
@@ -0,0 +1,41 @@
+import 'package:appro_chat/core/theme/svg_icon.dart';
+
+Map<String, dynamic> aiList = {
+  "1": {
+    "name": "تفسیروترجمه قرآن",
+    "image": SvgIcon.quran,
+  },
+  "2": {
+    "name": "پرسش و پاسخ شرعی",
+    "image": SvgIcon.quranQuestions,
+  },
+  "3": {
+    "name": "پیشنهاد غذا",
+    "image": SvgIcon.chef,
+  },
+  "4": {
+    "name": "عوارض و کاربرد دارو",
+    "image": SvgIcon.drug,
+  },
+  "5": {
+    "name": "تولید متن تبلیغاتی",
+    "image": SvgIcon.instagramAdsPost,
+  },
+  "6": {
+    "name": "تولید متن پست شخصی",
+    "image": SvgIcon.instagramUserText,
+  },
+  "7": {
+    "name": "هشتگ های ترند",
+    "image": SvgIcon.topHashtag,
+  },
+  "8": {
+    "name": "پیشنهاد فیلم و سریال",
+    "image": SvgIcon.filmSuggestion,
+  },
+  "9": {
+    "name": "پیشنهاد کتاب",
+    "image": SvgIcon.bookSuggestion,
+  }
+  // افزودن مدل‌های بیشتر در صورت نیاز
+};
